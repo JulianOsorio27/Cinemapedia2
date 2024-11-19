85dadfaac107bd019854a14a161cfa3de04e8da2
Julian Osorio <jaosoriom27@gmail.com>
2024-11-19 17:52:58 -0500

    seccion 16 terminada

---
+++ lib/presentation/views/movies/favorites_view.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class FavoriteView extends StatelessWidget {
+  const FavoriteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorite')
+      ),
+      body: Center( child: Text('Favorite'), )
+    );
+  }
+}
